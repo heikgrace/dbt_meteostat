4b3f0ceef0faf882dfc206ee263e947482e32aaf
Heiko Graceffa <heiko.graceffa@it-tem.de>
2025-07-15 10:18:43 +0200

    added marts_route_stats.sql from exercise 5.2

---
+++ models/mart/mart_route_stats.sql
@@ -0,0 +1,41 @@
+WITH flights_cleaned AS (
+    SELECT * 
+    FROM {{ ref('prep_flights') }}
+),
+airports AS (
+    SELECT faa, city, country, name
+    FROM {{ ref('prep_airports') }}
+),
+route_stats AS (
+    SELECT
+        origin,
+        dest,
+        COUNT(*) AS total_flights, -- total number of flights
+        COUNT(DISTINCT tail_number) AS unique_airplanes, -- number of unique airplanes
+        COUNT(DISTINCT airline) AS unique_airlines, -- number of unique airlines
+        ROUND(AVG(actual_elapsed_time), 2) AS avg_actual_elapsed_time, -- average actual flight time
+        ROUND(AVG(arr_delay), 2) AS avg_arrival_delay, -- average arrival delay
+        MAX(arr_delay) AS max_arrival_delay, -- maximum arrival delay
+        MIN(arr_delay) AS min_arrival_delay, -- minimum arrival delay
+        SUM(CASE WHEN cancelled = 1 THEN 1 ELSE 0 END) AS total_cancelled, -- total cancelled flights
+        SUM(CASE WHEN diverted = 1 THEN 1 ELSE 0 END) AS total_diverted -- total diverted flights
+    FROM flights_cleaned
+    GROUP BY origin, dest
+),
+joined_with_airports AS (
+    SELECT 
+        rs.*,
+        ao.city AS origin_city,
+        ao.country AS origin_country,
+        ao.name AS origin_airport_name,
+        ad.city AS dest_city,
+        ad.country AS dest_country,
+        ad.name AS dest_airport_name
+    FROM route_stats rs
+    LEFT JOIN airports ao ON rs.origin = ao.faa
+    LEFT JOIN airports ad ON rs.dest = ad.faa
+)
+
+SELECT *
+FROM joined_with_airports
+ORDER BY total_flights DESC;
